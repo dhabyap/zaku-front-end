151e9a954d6f34e7dfa19877e1d714825cde369c
dhabyap <dhabyap@gmail.com>
2026-08-03 13:32:22 +0700

    fix: register Alpine components in app.js (transactionDetail not defined)

--- dompet-frontend/resources/js/app.js
+++ dompet-frontend/resources/js/app.js
@@ -3,10 +3,13 @@ import apiClient from './api-client';
 import * as auth from './auth';
 import * as utils from './utils';
 import Alpine from 'alpinejs';
+import registerComponents from './alpine-components';
 
 window.apiClient = apiClient;
 window.auth = auth;
 window.utils = { ...window.utils, ...utils };
 window.Alpine = Alpine;
+
+registerComponents(Alpine);
 
 Alpine.start();
